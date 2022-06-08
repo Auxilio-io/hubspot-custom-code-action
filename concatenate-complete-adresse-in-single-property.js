963b8f4d135b6c765885e6dea05ae03491fde3cd
louischausse-auxilio <85704673+louischausse-auxilio@users.noreply.github.com>
2022-06-08 09:56:37 -0400

    Added comments to explein the purpose of code

--- concatenate-complete-adresse-in-single-property.js
+++ concatenate-complete-adresse-in-single-property.js
@@ -1,3 +1,7 @@
+// This code gets all the address properties (Street address, City, Region, Country, Postal code) and concatenates them into a single property named "Complete Address".
+// Useful when you want to display the complete address both on the sidebar or card of a contact record, and you are limited by the number of properties you can use.
+// This can also be extended to be used as a personalization token in emails, pages, quotes, etc.
+
 exports.main = (event, callback) => {
 
     const address = event.inputFields['address'];
